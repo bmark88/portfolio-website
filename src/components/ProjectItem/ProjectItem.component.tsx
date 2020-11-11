0387849eb791a6f2fa1eb175044bb8e2d2b163e5
bmark88 <bradley.mark@hotmail.com>
2020-11-11 16:45:31 -0500

    Add descriptions to projectListData, and rearrange basic layout

--- src/components/ProjectItem/ProjectItem.component.tsx
+++ src/components/ProjectItem/ProjectItem.component.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import './ProjectItem.styles.scss';
 
 const ProjectItem = (props: any) => {
-  const { title, imageUrl, link, id } = props;
+  const { title, imageUrl, link, id, description } = props;
   
   return (
-    <div className={id % 2 === 0 ? 'left' : 'right'}>
-      <h3>
-        <a href={link} target='_blank'>{title}</a>
-      </h3>
+    <div className={`${id % 2 === 0 ? 'left' : 'right'} project-item`}>
+      <div className='project-information-container'>
+        <h3>
+          <a href={link} target='_blank'>{title}</a>
+        </h3>
+        <p>{description}</p>
+      </div>
       <img src={imageUrl} alt='profile-picture' />
     </div>
   );
--- src/components/Projects/projectListData.ts
+++ src/components/Projects/projectListData.ts
@@ -10,37 +10,43 @@ const projectList = [
     id: 1,
     title: 'Redux Online Shop',
     link: 'https://redux-online-shop.herokuapp.com/',
-    imageUrl: reduxShop
+    imageUrl: reduxShop,
+    description: 'An e-commerce website using Firebase for authentication and Redux for state management. Users can check out their cart using the Stripe API.'
   },
   {
     id: 2,
     title: 'DevCollabs',
     link: 'https://dev-collabs.netlify.app/',
-    imageUrl: devCollabs 
+    imageUrl: devCollabs,
+    description: 'A collaboration app for developers to easily form their own hackathon groups, and live code with each other.'
   },
   {
     id: 3,
     title: 'Interview Scheduler',
     link: 'https://bootcamp-scheduler.netlify.app/', 
-    imageUrl: bootcampScheduler
+    imageUrl: bootcampScheduler,
+    description: 'A single-page application allowing users to schedule or delete interviews based on their availability.'
   },
   {
     id: 4,
     title: 'TinyApp',
     link: 'https://tiny-uri-app.herokuapp.com/', 
-    imageUrl: tinyApp
+    imageUrl: tinyApp,
+    description: 'Allows users to shorten long URLs (similar to bit.ly). There are custom tracking analytics to track visitors via their Visitor ID and timestamp.'
   },
   {
     id: 5,
     title: 'SwiftChat',
     link: 'https://swift-chat-app.herokuapp.com/',
-    imageUrl: swiftChat
+    imageUrl: swiftChat,
+    description: 'A family-friendly chat app that prohibits profanity, made for users to quickly communicate in dedicated rooms.'
   },
   {
     id: 6,
     title: 'Tweeter',
     link: 'https://tweeter-board.herokuapp.com/', 
-    imageUrl: tweeter
+    imageUrl: tweeter,
+    description: 'A single-page AJAX-based Twitter clone where users can create new tweets within the tweet board.'
   }
 ];
 
