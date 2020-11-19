d0f13efc1cf4005c1b8d92cc8316eaa141937832
bmark88 <bradley.mark@hotmail.com>
2020-11-19 16:40:47 -0500

    Setup contact form to email me directly, update contact form styles

--- src/components/ContactForm/ContactForm.component.tsx
+++ src/components/ContactForm/ContactForm.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormControl, Button, TextField } from '@material-ui/core';
 import './ContactForm.styles.scss';
 
@@ -14,27 +14,98 @@ const styles = {
   }
 };
 
-const ContactForm = () => (
-  <section id='contact'>
-    <div className='contact-form'>
-      <h3>Contact Me</h3>
-      <div className='form-content'>
-      <FormControl>
-        <TextField label='Your Name' required />
-        <TextField label='Your Phone Number' required />
-        <TextField label='Your Email' required />
-        <TextField 
-          multiline={true} 
-          variant='outlined' 
-          rows={5} 
-          label='Message' 
-          style={styles.additionalInfo} 
-        />
-        <Button variant='contained'>Send Message</Button>
-      </FormControl>
+const ContactForm = () => {
+  const [formInfo, setFormInfo] = useState({
+    name: '',
+    phoneNumber: '',
+    email: '',
+    message: ''
+  });
+
+  const { name, phoneNumber, email, message } = formInfo;
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+
+    const messageContent = `Hi Bradley, %0d%0a 
+      
+    %0d%0a ${message} %0d%0a %0d%0a 
+
+Please contact me at: %0d%0a %0d%0a 
+Email: ${email}
+%0d%0a or %0d%0a 
+Phone: ${phoneNumber}
+`;
+
+    const subjectContent = `
+      ${name} | Requesting More Information
+    `;
+  
+    window.open(`mailto:bradley.mark@hotmail.com?subject=${subjectContent}&body=${messageContent}`);
+
+    setFormInfo({
+      ...formInfo, 
+      name: '', 
+      phoneNumber: '', 
+      email: '', 
+      message: ''
+    });
+  };
+  
+  const handleChange = (e: any) => {
+    if (e.target.id) {
+      setFormInfo({...formInfo, [e.target.id]: e.target.value})
+    }
+  };  
+
+  return (
+    <section id='contact'>
+      <div className='contact-form'>
+        <h3>Contact Me</h3>
+        <div className='form-content'>
+        <FormControl>
+          <TextField 
+            label='Your Name' 
+            required 
+            onChange={handleChange}
+            value={name}
+            id='name'
+          />
+          <TextField 
+            label='Your Phone Number' 
+            required 
+            onChange={handleChange}
+            value={phoneNumber}
+            id='phoneNumber'
+          />
+          <TextField 
+            label='Your Email' 
+            required 
+            onChange={handleChange}
+            value={email}
+            id='email'
+          />
+          <TextField 
+            multiline={true} 
+            variant='outlined' 
+            rows={5} 
+            label='Message'
+            style={styles.additionalInfo} 
+            onChange={handleChange}
+            value={message}
+            id='message'
+          />
+          <Button 
+            variant='contained' 
+            onClick={handleClick}
+          >
+            Send Message
+          </Button>
+        </FormControl>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+}
 
 export default ContactForm;
